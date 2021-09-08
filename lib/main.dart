8fdf69682bbc1e6a17f7650ce139384b27a87aa0
Manish Parihar <pariharmanish5000@gmail.com>
2021-09-08 13:26:12 +0530

    description

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:practiceui/pages/description.dart';
 import 'package:practiceui/pages/homepage.dart';
 import 'package:practiceui/pages/mainhome.dart';
 void main()
@@ -10,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold( body:Mainhome()),
+      home: Scaffold( body:Description()),
     );
   }
 }
--- lib/pages/description.dart
+++ lib/pages/description.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 class Description extends StatefulWidget {
-  const Description({ Key? key }) : super(key: key);
+  
 
   @override
   _DescriptionState createState() => _DescriptionState();
@@ -10,10 +10,34 @@ class _DescriptionState extends State<Description> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        
-      ),
-      
+      backgroundColor: Colors.white,
+      body: Stack(
+        alignment: AlignmentDirectional.bottomCenter,
+        children: [
+          
+            
+             Container(
+          height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                 gradient: LinearGradient(colors: [Color(0xFFf58056),Color(0xFF2c77c7)],end: Alignment.bottomRight,begin: Alignment.topLeft,)
+                ),
+            ),
+          
+          
+             Container(
+              height: 620,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(topLeft: Radius.circular(20),topRight: Radius.circular(20)),
+                  color: Colors.white
+                ),),
+          Positioned(
+            top: 142,
+            right: 45,
+            child: Icon(Icons.notifications,color: Colors.black,size: 40,)),
+        ],)
     );
+      
+    
   }
 }
